d58b45cbc052aae865239a8d15ca97cd58cde896
kabirsingh201 <125553934+kabirsingh201@users.noreply.github.com>
2023-08-18 23:15:53 +0530

    Add files via upload

---
+++ largest.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int max;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    max=arr[0];
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    
+    cout<<max;
+    
+    return 0;
+}
